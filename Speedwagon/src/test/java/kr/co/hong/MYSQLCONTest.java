94ee0c7290c07e63ac4ddbf7561c67894dac2120
Kicky4damon <Dev4damon@gmail.com>
2023-08-25 15:41:15 +0900

    JDBC 커넥트 확인

---
+++ Speedwagon/src/test/java/kr/co/hong/MYSQLCONTest.java
@@ -0,0 +1,35 @@
+package kr.co.hong;
+
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+
+public class MYSQLCONTest {
+
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/book_ex?useSSL=false";
+
+	private static final String User = "root";
+
+	private static final String PW = "12345";
+
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName(DRIVER);
+
+		try(Connection con = DriverManager.getConnection(URL, User, PW)){
+			System.out.println(con);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+		
+	}
+	
+}
+
+
