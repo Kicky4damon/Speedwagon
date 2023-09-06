79bb533481dff82909dfe4f3863c9759dda47603
Kicky4damon <Dev4damon@gmail.com>
2023-09-06 17:58:42 +0900

    dummy 파일 수정

--- Speedwagon/src/main/java/kr/co/hong/HomeController.java
+++ Speedwagon/src/main/java/kr/co/hong/HomeController.java
@@ -35,5 +35,17 @@ public class HomeController {
 		
 		return "home";
 	}
-	
+	@RequestMapping(value = "/dummy", method = RequestMethod.GET)
+	public String dummy(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "dummy";
+	}
 }
